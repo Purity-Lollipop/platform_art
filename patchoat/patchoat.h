e670506267cc41af989471bbe1ac681fca364cae
AndroidRul3z <ferrettidario@gmail.com>
2015-05-23 15:59:45 +0200

    Revert "ART: prevent patchoat from symlinking with dummy files"
    This reverts commit e4a092dc818911c8fa789d8dc1383283f367cbc7.

--- patchoat/patchoat.h
+++ patchoat/patchoat.h
@@ -44,8 +44,7 @@ class PatchOat {
   // Patch only the oat file
   static bool Patch(File* oat_in, off_t delta, File* oat_out, TimingLogger* timings,
                     bool output_oat_opened_from_fd,  // Was this using --oatput-oat-fd ?
-                    bool new_oat_out,                // Output oat was a new file created by us?
-                    bool input_oat_filename_dummy);  // Input cannot be symlinked
+                    bool new_oat_out);               // Output oat was a new file created by us?
 
   // Patch only the image (art file)
   static bool Patch(const std::string& art_location, off_t delta, File* art_out, InstructionSet isa,
@@ -56,8 +55,7 @@ class PatchOat {
                     off_t delta, File* oat_out, File* art_out, InstructionSet isa,
                     TimingLogger* timings,
                     bool output_oat_opened_from_fd,  // Was this using --oatput-oat-fd ?
-                    bool new_oat_out,                // Output oat was a new file created by us?
-                    bool input_oat_filename_dummy);  // Input cannot be symlinked
+                    bool new_oat_out);               // Output oat was a new file created by us?
 
  private:
   // Takes ownership only of the ElfFile. All other pointers are only borrowed.
@@ -89,11 +87,10 @@ class PatchOat {
 
   // Attempt to replace the file with a symlink
   // Returns false if it fails
-  static bool SymlinkOrCopy(File* input_oat,
-                            File* output_oat,
-                            bool output_oat_opened_from_fd,
-                            bool new_oat_out,  // Output oat was newly created?
-                            bool make_copy);
+  static bool ReplaceOatFileWithSymlink(const std::string& input_oat_filename,
+                                        const std::string& output_oat_filename,
+                                        bool output_oat_opened_from_fd,
+                                        bool new_oat_out);  // Output oat was newly created?
 
   static void BitmapCallback(mirror::Object* obj, void* arg)
       SHARED_LOCKS_REQUIRED(Locks::mutator_lock_) {
